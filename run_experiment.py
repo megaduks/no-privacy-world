b1f6c7fc27cb374e61f590be33bb86d138f4fc23
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2019-12-19 22:39:46 +0100

    Changed ranges of parameters for the simulation

--- run_experiment.py
+++ run_experiment.py
@@ -1,25 +1,26 @@
 from tqdm import tqdm
+from datetime import datetime as dt
 import pandas as pd
 
 from NoPrivacyModel import TransactionModel
 
 if __name__ == '__main__':
 
-    n_iterations = 50
+    n_iterations = 1000
     n_plebeians = 100
     n_patricians = 100
-    file_name = f'results.csv'
+    file_name = f'results-{dt.now().timestamp()}.csv'
 
     mu_ranges = [ (-1,1) ]
-    sigma_ranges = [ (1) ]
-    alphas = [ 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8 ]
-    betas = [ 1.1, 1.5, 2, 3 ]
+    sigma_ranges = [ i/10 for i in range(1,15) ]
+    alphas = [ i/100 for i in range(50,90) ]
+    betas = [ 1.1, 1.5, 2, 3, 5 ]
 
     results = []
 
     for mu in mu_ranges:
-        for sigma in sigma_ranges:
-            for alpha in tqdm(alphas):
+        for sigma in tqdm(sigma_ranges):
+            for alpha in alphas:
                 for beta in betas:
                     for symmetric in [True]:
 
